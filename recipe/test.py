96cdb63067d3957172e258d7ca645b0a4bc4145d
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-09-16 15:59:23 -0500

    Improve test

--- recipe/test.py
+++ recipe/test.py
@@ -1,5 +1,10 @@
-from openff.nagl_models import validate_nagl_model_path, list_available_nagl_models
+from openff.nagl_models import validate_nagl_model_path, list_available_nagl_models, __version__
+from packging.version import Version
 
+
+assert Version(__version__) > Version("2026")
+
+print(list_available_nagl_models())
 
 assert len(list_available_nagl_models()) > 0
 
